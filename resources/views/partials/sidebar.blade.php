33cd4b3a1e3b371b20b8c1d63524fe71cea19393
shrimon347 <shrimon1999@gmail.com>
2025-02-23 03:11:14 +0600

    feat(dashbord): add dashbord some items

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -28,15 +28,18 @@
         role="menu"
         data-accordion="false"
         >
-        <li class="nav-item menu-open">
-            <a href="#" class="nav-link active">
-            <i class="nav-icon bi bi-speedometer"></i>
-            <p>
-                Dashboard
-                <i class="nav-arrow bi bi-chevron-right"></i>
-            </p>
-            </a>
-            <ul class="nav nav-treeview">
+        <li class="nav-item ">
+            
+            <ul class="nav  nav-link active">
+            <li class="nav-item">
+                <a href="{{('dashboard')}}" class="nav-link">
+                <i class="nav-icon bi bi-circle"></i>
+                <p>Dashboard</p>
+                </a>
+            </li>
+
+            </ul>
+            <ul class="nav  nav-link active">
             <li class="nav-item">
                 <a href="{{route('products.index')}}" class="nav-link">
                 <i class="nav-icon bi bi-circle"></i>
